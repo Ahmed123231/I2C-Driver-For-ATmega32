9d84512fcb62baff1f18aa1ad4e25dfa51a2c661
Ahmed Atef <zarakikenbachi28@gmail.com>
2024-08-13 08:35:10 +0300

    V1.0
    First

---
+++ TWI/TWI/Debug/TWI_prog.d
@@ -0,0 +1,12 @@
+TWI_prog.d TWI_prog.o: .././TWI_prog.c .././BIT_MATH.h .././STD_TYPE.h \
+ .././TWI_private.h .././TWI_reg.h .././TWI_interface.h
+
+.././BIT_MATH.h:
+
+.././STD_TYPE.h:
+
+.././TWI_private.h:
+
+.././TWI_reg.h:
+
+.././TWI_interface.h:
